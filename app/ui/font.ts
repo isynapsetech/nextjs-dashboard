aba7bf10b9357f552b5d03a4269e26695b8e7a85
Indranil <isynapsetech@gmail.com>
2025-07-31 19:02:45 +0100

    Initial Commit

---
+++ app/ui/font.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700'] });
